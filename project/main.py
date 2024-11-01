1039f3c1b9945f316b3273ca0564812ac1f5dbc5
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-02 00:21:12 +0100

    created scatterplot

--- project/main.py
+++ project/main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-housing_data = pd.read_csv("project/dataset/Housing.csv")
+df = pd.read_csv("/Users/mathiasnord/Desktop/housing_price_project/project/dataset/Housing.csv")
+
+plt.figure(figsize=(10, 6))
+plt.scatter(df.iloc[:, 1], df.iloc[:, 0], alpha = 0.5)
+plt.title("Housing price vs area")
+plt.xlabel("Area (sqft)")
+plt.ylabel("Price")
+plt.grid(True)
+plt.show()
 
